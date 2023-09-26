19bf65cd7b211e701d259af23b4bfd862cad252f
GeekyBecky <beckywu310@gmail.com>
2023-09-25 23:10:09 -0700

    first commit

---
+++ app/api/openai/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+export async function POST(request: Request, response: any) {
+  try {
+    const { title, role } = await request.json();
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Create 3 paragraph blog post with html tags based on this title: ${title}`,
+        },
+        {
+          role: "system",
+          content: `${
+            role || "I am a helpful assistant"
+          }. Write with html tags.`,
+        },
+      ],
+    });
+    //response.revalidate("/api/posts");
+    return NextResponse.json(
+      {
+        content: completion.choices[0].message?.content,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("request error", error);
+    NextResponse.json({ error: "error updating post" }, { status: 500 });
+  }
+}
---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import "./globals.css";
+import { Open_Sans } from "next/font/google";
+import Navbar from "app/(shared)/Navbar";
+import Footer from "app/(shared)/Footer";
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Blog AI App",
+  description: "Blog built in Next JS that uses AI",
+};
+// metadata is an object that contains the title and description of the page. it is used in the head of the page
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html className={openSans.className} lang="en">
+
+      <body>
+      <Navbar />
+        {children}
+      <Footer />
+      </body>
+      
+    </html>
+  );
+}
